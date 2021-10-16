9b2af43cffd62181ff881d9f74dbbc49d777bda7
Sara583 <77129891+Sara583@users.noreply.github.com>
2021-10-17 01:03:17 +0200

    add gitignore, procfile and update parts

--- app.js
+++ app.js
@@ -117,9 +117,12 @@ if(listName==="Today"){
   });
 
 
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 3000;
+  }
 
 
-
-  app.listen(3000,function(){
+  app.listen(port,function(){
     console.log("Server started working on port 3000")
   });
